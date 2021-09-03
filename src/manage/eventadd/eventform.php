0782ee7b2a16452c9d90420e7224404ca6cb8c87
moripa115 <70812517+moripa115@users.noreply.github.com>
2021-09-03 19:45:32 +0900

    #21/#22完成

---
+++ src/manage/eventadd/eventform.php
@@ -0,0 +1,34 @@
+<?php require('../../dbconnect.php');
+
+$id = $_GET['id'];
+?>
+<?php if (isset($id)) { ?>
+  <?php
+  // <!-- 🍅と結合する -->
+  $detail_contents_value = "SELECT* FROM events INNER JOIN event_details ON events.id = event_details.event_id WHERE events.id = $id";
+  $detail_contents = $db->query($detail_contents_value)->fetch();
+  ?>
+<?php } ?>
+
+<div class="event__add">
+  <form class="event__add__form" action="/manage/eventadd/eventadd.php" method="post">
+    イベント名
+    <input class="event__add__form__event__place event__add__form__item" type="textarea" name="name" value="<?= $detail_contents['name'] ?>">
+    開始日時
+    <input placeholder="2020-08-09" class="event__add__form__event__date event__add__form__item" type="text" name="start_at" value="<?= $detail_contents['start_at'] ?>">
+    終了日時
+    <input placeholder="2020-08-09" class="event__add__form__event__date event__add__form__item" type="text" name="end_at" value="<?= $detail_contents['end_at'] ?>">
+      イベント詳細
+      <textarea class="event__add__form__event__detail" name="text" rows="7" cols="150"><?= $detail_contents["text"] ?></textarea>
+      <input class="event__add__form__button" type="submit" value="送信">
+      <input type="hidden" value="<?= $id ?>" name="id">
+  </form>
+</div>
+<?= $_POST['event_name'] ?>
+<hr>
+<div class="event__add__back">
+  <a class="event__add__top__back__button" href="../index.php">トップへ戻る</a>
+  <a class="event__add__event__list__back__button" href="/admin_page/event_add_list/">追加したイベントの一覧ページに戻る</a>
+</div>
+</body>
+</html>
