19bcf3cb75567e285db6c997aa1788d9182a2c46
ryotaro23 <ryo2017taro@icloud.com>
2021-09-05 01:52:34 +0900

    #91 ページャーきれいにしました

--- src/manage/eventlist/index.php
+++ src/manage/eventlist/index.php
@@ -77,48 +77,47 @@ $disp_data = array_slice($event_contents, $start_no, MAX, true);
     $end_date = strtotime($event['end_at']);
     $day_of_week = get_day_of_week(date("w", $start_date));
     ?>
-    <div class="modal-open bg-white mb-3 p-4 flex justify-between rounded-md shadow-md cursor-pointer" id="event-<?php echo $event['id']; ?>">
-      <div>
-        <h3 class="font-bold text-lg mb-2"><?php echo $event['name'] ?></h3>
-        <p><?php echo date("Y年m月d日（${day_of_week}）", $start_date); ?></p>
-        <p class="text-xs text-gray-600">
-          <?php echo date("H:i", $start_date) . "~" . date("H:i", $end_date); ?>
-        </p>
-      </div>
-      <div class="flex flex-col justify-between text-right">
+      <div class="modal-open bg-white mb-3 p-4 flex justify-between rounded-md shadow-md cursor-pointer" id="event-<?php echo $event['id']; ?>">
         <div>
-          <?php if ($event['id'] % 3 === 1) : ?>
-            <!--
-            <p class="text-sm font-bold text-yellow-400">未回答</p>
-            <p class="text-xs text-yellow-400">期限 <?php echo date("m月d日", strtotime('-3 day', $end_date)); ?></p>
-            -->
-          <?php elseif ($event['id'] % 3 === 2) : ?>
-            <!-- 
-            <p class="text-sm font-bold text-gray-300">不参加</p>
-            -->
-          <?php else : ?>
-            <!-- 
-            <p class="text-sm font-bold text-green-400">参加</p>
-            -->
-          <?php endif; ?>
+          <h3 class="font-bold text-lg mb-2"><?php echo $event['name'] ?></h3>
+          <p><?php echo date("Y年m月d日（${day_of_week}）", $start_date); ?></p>
+          <p class="text-xs text-gray-600">
+            <?php echo date("H:i", $start_date) . "~" . date("H:i", $end_date); ?>
+          </p>
         </div>
-        <p class="text-sm"><span class="text-xl"><?php echo $event['total_participants']; ?></span>人参加 ></p>
+        <div class="flex flex-col justify-between text-right">
+          <div>
+            <?php if ($event['id'] % 3 === 1) : ?>
+              <!--
+              <p class="text-sm font-bold text-yellow-400">未回答</p>
+              <p class="text-xs text-yellow-400">期限 <?php echo date("m月d日", strtotime('-3 day', $end_date)); ?></p>
+              -->
+            <?php elseif ($event['id'] % 3 === 2) : ?>
+              <!-- 
+              <p class="text-sm font-bold text-gray-300">不参加</p>
+              -->
+            <?php else : ?>
+              <!-- 
+              <p class="text-sm font-bold text-green-400">参加</p>
+              -->
+            <?php endif; ?>
+          </div>
+          <p class="text-sm"><span class="text-xl"><?php echo $event['total_participants']; ?></span>人参加 ></p>
+        </div>
       </div>
-    </div>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+        <div class = "pager-num">
+      <?php
+      for($i = 1; $i <= $max_page; $i++){ // 最大ページ数分リンクを作成
+          if ($i == $now) {?>
+            <span class = "blue-word page-number"> <?php echo $now. '　'; ?> </span>
+        <?php  } else {
+              echo '<a class = "page-number" href="/manage/eventlist/index.php?page_id='. $i. '")>'. $i. '</a>';?>
+        <?php }
+      }?>
+      </div>
+  
 
- <?php
-for($i = 1; $i <= $max_page; $i++){ // 最大ページ数分リンクを作成
-    if ($i == $now) { // 現在表示中のページ数の場合はリンクを貼らない
-        echo $now. '　'; 
-    } else {
-        echo '<a href="/manage/eventlist/index.php?page_id='. $i. '")>'. $i. '</a>';
-    }
-}
- 
-?>
-      </div>
-    </div>
   </main>
 
   <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
