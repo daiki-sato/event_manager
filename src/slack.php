991403c59604f17e0f7a24ec092030c2645d7136
shuto0410 <69708307+shuto0410@users.noreply.github.com>
2021-09-04 13:51:52 +0900

    とりあえずスラック動くよ

---
+++ src/slack.php
@@ -0,0 +1,22 @@
+<?php
+$url = "https://hooks.slack.com/services/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";
+$message = [
+    'channel' => '#チャンネル名',
+    'username' => 'System',
+    'icon_emoji' => ':eyes:',
+    'text' => 'これはテストです<@U01C72Q45MJ>',
+];
+
+$ch = curl_init();
+$options = [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query([
+        'payload' => json_encode($message)
+    ])
+];
+curl_setopt_array($ch, $options);
+curl_exec($ch);
+curl_close($ch);
