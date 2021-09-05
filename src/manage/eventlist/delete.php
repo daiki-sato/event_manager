1c85035c0cc48eb34a05faee3eb21c774d4549c5
ryotaro23 <ryo2017taro@icloud.com>
2021-09-05 09:59:58 +0900

    #109管理者画面で削除ボタンを押したときの変移先のページ作成

--- src/manage/eventlist/delete.php
+++ src/manage/eventlist/delete.php
@@ -4,10 +4,42 @@ include($_SERVER['DOCUMENT_ROOT'] . "/dbconnect.php");
 // イベント削除
 try {
     $stmt = $db->prepare('DELETE FROM events WHERE id = :id');
-    $stmt->execute(array(':id' => $_GET["delete"]));
+    $stmt->execute(array(':id' => $_GET["delete"]));?>
 
-    echo "削除しました。";
-} catch (Exception $e) {
+    <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Schedule | POSSE</title>
+  <link rel="stylesheet" href="delete.css">
+</head>
+
+<body>
+  <header class="h-16">
+    <div class="flex justify-between items-center w-full h-full mx-auto pl-2 pr-5">
+      <div class="h-full">
+      <a href="/manage/eventlist/index.php"><img src="/img/header-logo.png" alt="posseロゴ" class="h-full"></a> 
+      </div>
+      <div class="box1">
+        <div class="box px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white"><a href="/manage/eventlist/index.php">イベント一覧</a></div>
+        <div class="box px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white"><a href="/manage/eventadd/eventform.php">イベント追加</a></div>
+        <div class="box px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white"><a href="/auth/signup">ユーザー追加</a></div>
+      </div>
+    </div>
+  </header>
+  <main class="bg-gray-100 h-screen">
+   <h1>削除完了しました！</h1>
+   <img class="checked" src="/img/checked.png" alt="">
+  </main>
+</body>
+
+</html>
+
+<?php } catch (Exception $e) {
     echo 'エラーが発生しました。:' . $e->getMessage();
 }
 ?>
