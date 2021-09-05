cf703dddcce4378302f937e0b373f2112cc434e5
moripa115 <70812517+moripa115@users.noreply.github.com>
2021-09-05 10:34:05 +0900

    ログアウト後のページを見た目

--- src/session/logout.php
+++ src/session/logout.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-$output = '';
-if (isset($_SESSION["ID"])) {
-    $output = 'Logoutしました。';
+// $output = '';
+// if (isset($_SESSION["ID"])) {
+//     $output = 'Logoutしました。';
 
-} else {
-    $output = 'Loginしてません。';
-}
+// } else {
+//     $output = 'Loginしてません。';
+// }
 //セッション変数のクリア
 $_SESSION = array();
 //セッションクッキーも削除
@@ -26,4 +26,35 @@ if (ini_get("session.use_cookies")) {
 @session_destroy();
 
 echo $output;
-echo '<a href="/auth/login";>loginページへ戻る</a>';
+// echo '<a href="/auth/login";>loginページへ戻る</a>';?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Schedule | POSSE</title>
+  <link rel="stylesheet" href="logout.css">
+</head>
+
+<body>
+  <header class="h-16">
+    <div class="flex justify-between items-center w-full h-full mx-auto pl-2 pr-5">
+      <div class="h-full">
+      <a href="/manage/eventlist/index.php"><img src="/img/header-logo.png" alt="posseロゴ" class="h-full"></a> 
+      </div>
+      <div class="box1">
+      <a href="/auth/login/index.php" class="text-white bg-blue-400 px-4 py-2 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-200">ログイン</a>
+      </div>
+    </div>
+  </header>
+  <main class="bg-gray-100 h-screen">
+   <h1>Log Outしました。</h1>
+   <img class="checked" src="/img/checked.png" alt="">
+  </main>
+</body>
+
+</html>
